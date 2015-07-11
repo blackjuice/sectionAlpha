1f615f6b758d2704bf63a158d7b21fd0d8e5f8ec
BlackJuice <lucas.sj.hong@gmail.com>
2015-07-11 15:27:30 -0300

    tile \n ===

--- RegularExpression/Java/RSTtoMD/Test001.java
+++ RegularExpression/Java/RSTtoMD/Test001.java
@@ -1,6 +1,6 @@
 public class Test001 {
 
-    private static String re_00 = "====+|----+";         // === || ---
+    private static String re_00 = "===+|---+";         // === || ---
     private static String re_01 = "(\\.\\.) [a-z]+::";     // .. toctree::
     private static String re_02 = " +:[a-z]+: [1-9]+";            // :maxdepth:
     
@@ -22,21 +22,27 @@ public class Test001 {
             StdOut.println("<i>previous filename: " + args[i] + "</i>");
             // store previous line
             String prev_line = in.readLine();
+            StdOut.println(prev_line); // printing Title
             while (true) {
                 // reading current line
                 String curr_line = in.readLine();
-
                 // if current line is null, end file and read next file
                 if (curr_line == null) break;
 
-                // if ..toctree::
-                if ( curr_line.matches(re_00) && !prev_line.equals(curr_line) ) {
-                    StdOut.println(prev_line + "\n" + curr_line);
+                // if === or ----
+                if ( curr_line.matches(re_00)) {
+                    StdOut.println( curr_line );
+                    //StdOut.println("** " + curr_line);
                 }
 
-                else if ( !curr_line.matches(re_01) && !curr_line.matches(re_02) ) {
-                    StdOut.println(curr_line);
-                    //StdOut.println(prev_line + "\n\n" + curr_line);
+                //else if (  prev_line.matches(re_00) ) {
+                //    StdOut.println("** " + prev_line);
+                //}
+                // if != ..toctree:: or :maxdepth:
+                else if ( !curr_line.matches(re_01) && !curr_line.matches(re_02) && !curr_line.matches(re_00) ) {
+                    curr_line = curr_line.replaceAll("\t|   +", ""); // replace tabs
+                    StdOut.println("curr > " + curr_line + "\n");
+                    //StdOut.println(curr_line + "\n");
                 }
 
                 prev_line = curr_line;
