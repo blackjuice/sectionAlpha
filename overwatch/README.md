35943792ae82cdcfde28515e79a09eb015886aba
BlackJuice <lucas.sj.hong@gmail.com>
2017-05-24 14:23:57 -0300

    1st commit reads and creates ddt from a csv file

---
+++ overwatch/README.md
@@ -0,0 +1,7 @@
+# Overwatch Stats
+
+*creation: 170524*
+
+The following contains overwatch statistics calculator.
+
+Coding written in **Python**.
---
+++ overwatch/ddt_build.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env 
+
+# import from default python libraries
+import csv
+import sys
+# import from custom
+from aux_print_py3 import *
+
+# auxilary functions
+
+# jump commentary lines on .csv file
+def jmpComment(datafileString):
+    i = 0
+    with open(datafileString) as infile:
+        for line in infile:
+            i += 1
+            if not (line).startswith("//"):
+                infile.close(); return i;
+
+# dictionary
+# test101
+def test1():
+    with open('data.csv') as infile:
+        reader = csv.DictReader(infile)
+        for row in reader:
+            print(row, "\n")
+
+# imported from ddt_builder151231
+def csv2ddt (datafileString):
+    i = jmpComment(datafileString)
+    with open(datafileString) as infile:
+    #with open('data.csv') as infile:
+        for j in range(i - 1): #i-1 if whiteline
+            line = infile.readline();
+        ddt = {}
+        for line in infile:
+            tmp_array = [x.strip() for x in (line.strip()).split(',')]
+            tmp_value = []
+            for i in range( 1, len(tmp_array) ):
+                tmp_value.insert(i, tmp_array[i])
+            ddt.update({tmp_array[0]:tmp_value})
+    infile.close()
+    return ddt
+
+# execution
+datafileString = 'data.csv'
+ddt = {}
+ddt = csv2ddt(datafileString)
+# printing test
+print (ddt)
+print_ddt (ddt)
+print_ddt_sorted_key (ddt)
+print_ddt_keys_SCH (ddt)
+print_file (datafileString)
